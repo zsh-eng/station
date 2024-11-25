a48a32b528a80b576f3ae011a51f130bfe0a7c4b
zsheng <zsheng.dev@gmail.com>
2024-11-25 21:26:46 +0800

    add shadcn/ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,45 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Greet } from '@wailsjs/go/main/App';
+import { useState } from 'react';
+import '@/App.css';
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
+  const [resultText, setResultText] = useState(
+    'Please enter your name below 👇'
+  );
+  const [name, setName] = useState('');
+  const updateName = (e: any) => setName(e.target.value);
+  const updateResultText = (result: string) => setResultText(result);
 
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
+  async function greet() {
+    const result = await Greet(name);
+    updateResultText(result);
+  }
 
-    return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
-        </div>
-    )
+  return (
+    <div
+      id='App'
+      className='flex flex-col items-center justify-center h-screen'
+    >
+      <div id='result' className='result'>
+        {resultText}
+      </div>
+      <div id='input' className='max-w-md mx-auto flex flex-col gap-2'>
+        <Input
+          id='name'
+          className='input'
+          onChange={updateName}
+          autoComplete='off'
+          name='input'
+          type='text'
+        />
+        <Button variant={'outline'} onClick={greet} className='w-full'>
+          Greet
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
